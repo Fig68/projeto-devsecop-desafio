60614839bc9b6798aa549eadc4bc7bf611fd3a98
Fig68 <trilhatrilhas@gmail.com>
2026-09-24 10:26:22 -0300

    fix: corrige vulnerabilidades do Semgrep e formata yaml

--- src/script.js
+++ src/script.js
@@ -1,31 +1,13 @@
-
-
-// Busca tarefas do "banco de dados"
-fetch('db.json')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('db-status').innerText = data.status;
-
-        const list = document.getElementById('task-list');
-        data.itens.forEach(item => {
-            let li = document.createElement('li');
-            li.innerText = item.task;
-            list.appendChild(li);
-        });
-    })
-    .catch(err => {        
-        document.getElementById('db-status').innerText =
-            'Erro interno: ' + err.stack;
-    });
-
 // Adiciona nova tarefa na tela
 function addTask() {
     const input = document.getElementById('new-task');
     const output = document.getElementById('output');
 
-    output.innerHTML = '<li>' + input.value + '</li>';
+    const li = document.createElement('li');
+    li.innerText = input.value;
+    output.appendChild(li);
 
-    eval('console.log("Tarefa adicionada: ' + input.value + '")');
+    console.log("Tarefa adicionada: " + input.value);
 
     input.value = '';
 }
